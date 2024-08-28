23926a81b944bc414ec6e8b36ca1e6636e78eddb
Iresh Dissanayaka <iresh@n-able.biz>
2024-08-28 14:25:37 +0000

    add env prepare scripts and .env file

--- env.sh
+++ env.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+source .env
+
 alias it=$PWD/tools/imagetool/bin/imagetool.sh
 alias ah=$PWD/tools/weblogic-deploy/bin/archiveHelper.sh
 wdt=$PWD/tools/weblogic-deploy.zip
+
+docker login -u $OCR_USERNAME -p $OCR_TOKEN $OCR_SERVER
